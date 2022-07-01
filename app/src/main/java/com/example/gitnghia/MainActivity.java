e244597d73871325cd377830d61c338db9265f29
DoTrungNghia <trungnghia1st@gmail.com>
2022-07-01 12:28:33 +0700

    Ánh xạ id 'text' textview và gán id với 1 chuỗi

--- app/src/main/java/com/example/gitnghia/MainActivity.java
+++ app/src/main/java/com/example/gitnghia/MainActivity.java
@@ -11,14 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mapping();
-//        text=(TextView)findViewById(R.id.text);
         text = findViewById(R.id.text);
         text.setText("Hi There!!!");
     }
-
-//    private void mapping() {
-////        text.findViewById(R.id.text);
-//        text=(TextView)findViewById(R.id.text);
-//    }
 }
